28b2d9c1bd291be6a7ff49d49ac956414add0a3f
Tom Carey <tom@cactuslab.com>
2018-08-30 21:28:06 +1200

    moved prepare to play

--- VBVMI/AppDelegate.swift
+++ VBVMI/AppDelegate.swift
@@ -145,14 +145,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         URLCache.shared.removeAllCachedResponses()
-        
+        SoundManager.sharedInstance.prepareToPlayAudio()
         logger.info("🍕Foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         logger.info("🍕Active")
-        SoundManager.sharedInstance.prepareToPlayAudio()
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
